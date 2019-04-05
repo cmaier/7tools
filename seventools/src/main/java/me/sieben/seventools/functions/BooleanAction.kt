be5964074c0539b0fe74846413a996d0067673b4
Christian Maier <cmaierrr@gmail.com>
2019-04-05 08:51:10 +0200

    Add nonnull Boolean actions (onTrue(), onFalse())

--- seventools/src/main/java/me/sieben/seventools/functions/BooleanAction.kt
+++ seventools/src/main/java/me/sieben/seventools/functions/BooleanAction.kt
@@ -1,7 +1,17 @@
 package me.sieben.seventools.functions
 
+inline infix fun Boolean.onTrue(action: () -> Unit): Boolean {
+    if (this) action()
+    return this
+}
+
 inline infix fun Boolean?.onTrue(action: () -> Unit): Boolean? {
     if (this != null && this) action()
+    return this
+}
+
+inline infix fun Boolean.onFalse(action: () -> Unit): Boolean {
+    if (!this) action()
     return this
 }
 
