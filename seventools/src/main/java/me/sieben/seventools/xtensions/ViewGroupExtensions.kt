ce69a97f20ccaa3aeb707ade41ee898676cc383a
Christian Maier <cmaier@users.noreply.github.com>
2019-04-05 08:20:47 +0200

    Update dependencies and migrate to AndroidX (#14)
    Update dependencies and migrate to AndroidX

--- seventools/src/main/java/me/sieben/seventools/xtensions/ViewGroupExtensions.kt
+++ seventools/src/main/java/me/sieben/seventools/xtensions/ViewGroupExtensions.kt
@@ -2,9 +2,9 @@
 
 package me.sieben.seventools.xtensions
 
-import android.support.annotation.LayoutRes
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, parent: ViewGroup? = this, attachToRoot: Boolean = false): View =
         context.layoutInflater.inflate(layoutRes, parent, attachToRoot)
