a1f76f46057135c547025448baac8046b85dbc88
Christian Maier <cmaierrr@gmail.com>
2019-11-26 19:43:24 +0100

    Deprecate letNotNull() functions

--- seventools/src/main/java/me/sieben/seventools/containers/MoreTuples.kt
+++ seventools/src/main/java/me/sieben/seventools/containers/MoreTuples.kt
@@ -4,6 +4,7 @@ package me.sieben.seventools.containers
 
 import java.io.Serializable
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 data class Quad<out A, out B, out C, out D>(val first: A,
                                             val second: B,
                                             val third: C,
@@ -12,8 +13,10 @@ data class Quad<out A, out B, out C, out D>(val first: A,
     override fun toString() = "($first, $second, $third, $fourth)"
 }
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 fun <T> Quad<T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth)
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 data class Quin<out A, out B, out C, out D, out E>(val first: A,
                                                    val second: B,
                                                    val third: C,
@@ -23,8 +26,10 @@ data class Quin<out A, out B, out C, out D, out E>(val first: A,
     override fun toString() = "($first, $second, $third, $fourth, $fifth)"
 }
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 fun <T> Quin<T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth)
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 data class Hex<out A, out B, out C, out D, out E, out F>(val first: A,
                                                          val second: B,
                                                          val third: C,
@@ -35,8 +40,10 @@ data class Hex<out A, out B, out C, out D, out E, out F>(val first: A,
     override fun toString() = "($first, $second, $third, $fourth, $fifth, $sixth)"
 }
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 fun <T> Hex<T, T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth, sixth)
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 data class Sep<out A, out B, out C, out D, out E, out F, out G>(val first: A,
                                                                 val second: B,
                                                                 val third: C,
@@ -48,4 +55,5 @@ data class Sep<out A, out B, out C, out D, out E, out F, out G>(val first: A,
     override fun toString() = "($first, $second, $third, $fourth, $fifth, $sixth, $seventh)"
 }
 
+@Deprecated("This n-tuples only existed for nTuple.letNotNull() which has been deprecated.", ReplaceWith(""))
 fun <T> Sep<T, T, T, T, T, T, T>.toList(): List<T> = listOf(first, second, third, fourth, fifth, sixth, seventh)
