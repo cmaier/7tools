5a60336272e965856286811e7903e882644a091c
Christian Maier <cmaier@users.noreply.github.com>
2020-03-18 21:00:23 +0100

    Kotlin Script for Gradle (#26)
    * Refactor to KTS
    
    * Update the Gradle Wrapper to 6.2.2
    
    * Update a few dependencies

---
+++ seventools/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.android.build.gradle.BaseExtension
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.github.dcendents.android-maven")
+}
+
+group = "com.github.cmaier"
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        minSdkVersion(19)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.70")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.fragment:fragment:1.2.3")
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2") {
+        exclude(group = "com.android.support", module = "support-annotations")
+    }
+    testImplementation("io.kotlintest:kotlintest:2.0.7")
+}
+
+tasks {
+    val sourcesJar = register<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(project.the<BaseExtension>().sourceSets["main"].java.srcDirs)
+    }
+    artifacts { add("archives", sourcesJar) }
+}
